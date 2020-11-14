bb93e42a2bc5d5d0897e6b7a4703363b7771ab39
musupadi <33887565+musupadi@users.noreply.github.com>
2020-11-15 06:58:54 +0700

    Pengubahan data

--- app/src/main/java/com/destinyapp/puskomdik/API/RetroServer.java
+++ app/src/main/java/com/destinyapp/puskomdik/API/RetroServer.java
@@ -7,7 +7,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-    private static final String base_url = "http://apikitabelajar.mitradanacepat.com/";
+    private static final String base_url = "http://apipuskomdik.mitradanacepat.com/";
     private static Retrofit retrofit;
 
     public static Retrofit getClient(){
