8d5b18c53552be9be925e9a6f9c620071f9edcfb
Musupadi <musupadi159@gmail.com>
2020-11-16 05:32:33 +0700

    update APK Puskomdik

--- app/src/main/java/com/destinyapp/puskomdik/Activity/ui/UserFragment.java
+++ app/src/main/java/com/destinyapp/puskomdik/Activity/ui/UserFragment.java
@@ -3,22 +3,32 @@ package com.destinyapp.puskomdik.Activity.ui;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
+import com.destinyapp.puskomdik.API.ApiRequest;
+import com.destinyapp.puskomdik.API.RetroServer;
+import com.destinyapp.puskomdik.Activity.HomeActivity;
 import com.destinyapp.puskomdik.Activity.LoginActivity;
 import com.destinyapp.puskomdik.Method.Destiny;
+import com.destinyapp.puskomdik.Model.ResponseModel;
 import com.destinyapp.puskomdik.R;
 import com.destinyapp.puskomdik.SharedPreferance.DB_Helper;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserFragment extends Fragment {
     Button logout;
@@ -77,11 +87,38 @@ public class UserFragment extends Fragment {
                 Photo = cursor.getString(5);
             }
         }
-        nama.setText(Nama);
-        NISN.setText(Username);
-        NISN2.setText(Username);
-        Glide.with(this)
-                .load(destiny.BASE_URL()+Photo)
-                .into(profile);
+
+    }
+    private void Logic(){
+        ApiRequest api = RetroServer.getClient().create(ApiRequest.class);
+        final Call<ResponseModel> login =api.login(Username,Password);
+        login.enqueue(new Callback<ResponseModel>() {
+            @Override
+            public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
+                try {
+                    if (response.body().getStatusCode().equals("000")){
+                        nama.setText(response.body().getData().get(0).getName());
+                        NISN.setText(Username);
+                        NISN2.setText(Username);
+                        Glide.with(getActivity())
+                                .load(destiny.BASE_URL()+response.body().getData().get(0).getPhoto())
+                                .into(profile);
+                        dbHelper.Logout();
+                        dbHelper.SaveUser(Username,Password,response.body().getData().get(0).getName(),response.body().getData().get(0).getAccessToken(),response.body().getData().get(0).getAs(),response.body().getData().get(0).getPhoto());
+                    }else{
+                        Toast.makeText(getActivity(), response.body().getStatusMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }catch (Exception e){
+                    Toast.makeText(getActivity(), "Terjadi Kesalahan "+e.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseModel> call, Throwable t) {
+//                Toast.makeText(LoginActivity.this, "Koneksi Gagal", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "Koneksi Gagal", Toast.LENGTH_SHORT).show();
+                Log.i("Login Logic : ",t.toString());
+            }
+        });
     }
 }
