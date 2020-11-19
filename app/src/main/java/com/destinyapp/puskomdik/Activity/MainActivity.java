196252d6000849710fe15764aa7fd5e888852c13
musupadi <33887565+musupadi@users.noreply.github.com>
2020-11-19 18:07:02 +0700

    Login And Splash

--- app/src/main/java/com/destinyapp/puskomdik/Activity/MainActivity.java
+++ app/src/main/java/com/destinyapp/puskomdik/Activity/MainActivity.java
@@ -2,21 +2,23 @@ package com.destinyapp.puskomdik.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.destinyapp.puskomdik.R;
 import com.destinyapp.puskomdik.SharedPreferance.DB_Helper;
 
 public class MainActivity extends AppCompatActivity {
-
+    LottieAnimationView lottie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Handler handler = new Handler();
+        lottie = findViewById(R.id.lottie);
         final DB_Helper dbHelper = new DB_Helper(MainActivity.this);
         Cursor cursor = dbHelper.checkUser();
         if (cursor.getCount()>0){
@@ -24,13 +26,29 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         }else{
-            handler.postDelayed(new Runnable() {
-                public void run() {
+            lottie.addAnimatorListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
                     Intent intent = new Intent(MainActivity.this,LoginActivity.class);
                     startActivity(intent);
                     finish();
                 }
-            }, 3000); //3000 L = 3 detik
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
         }
     }
 }
